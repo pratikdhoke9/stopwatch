d506489c0b4b5d47ddae70259dd52c7f963949d1
Pratik Dhoke <72990535+pratikdhoke9@users.noreply.github.com>
2023-01-28 14:12:01 +0530

    renewed index.js
    changed the input time type

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-// let beepsound = new Audio('https://orangefreesounds.com/clock-alarm-electronic-beep/');   
-//   beepsound.play()
-
+document.getElementById("hour").value='00'
+document.getElementById("minute").value='00'
+document.getElementById("second").value='00'
 
 var audio = document.getElementById("myAudio");
-function load(){
-  document.getElementById("time").value = "00:00:01";
-}
 
 function show(){
-  // document.getElementById("submit").style.visibility = "hidden";
   document.getElementById('submit').style.cssText = 'background-color: red; color: white; font-size: 35px';
+  document.getElementById('invalid').style.cssText = 'visibility: hidden'
   document.getElementById("submit").disabled = true;
-  time = document.getElementById("time").value.split(':');
-  countdown(time)
+  countdown()
 }
 
-function countdown(time){
-  hour= Number(time[0]),minute = Number(time[1]),second = Number(time[2])
-
-  myInterval = setInterval(function(){
-      
+function countdown(){
+  hour= Number(document.getElementById("hour").value)
+  minute = Number(document.getElementById("minute").value)
+  second = Number(document.getElementById("second").value)
+  if(minute>=60 || second>=60 || minute<0 || hour<0 || second<0){
+    document.getElementById('submit').style.cssText = 'background-color: green; color: white; font-size: 35px';
+    document.getElementById('invalid').style.cssText = 'color : red; visibility: visible'
+    document.getElementById("submit").disabled = false;
+  }
+  else{
+    myInterval = setInterval(function(){
       if(hour!=0 || minute!=0 || second!=0){
-        
         if(second==0 && minute>0){
           minute--;
           second = 60;
@@ -31,7 +32,6 @@ function countdown(time){
           minute=59
           second=60
         }
-        
         element = document.getElementById('screen')
         element.innerHTML = ((hour<10)?("0"+hour):hour)
             +":"+((minute<10)?"0"+minute:minute)
@@ -39,10 +39,9 @@ function countdown(time){
         second--
       }
       else{
-        document.getElementById('submit').style.cssText = 'background-color: green; color: white; font-size: 35px';
+        document.getElementById('submit').style.cssText = 'background-color: rgb(30, 234, 64); color: white; font-size: 35px';
         element = document.getElementById('screen')
         element.innerHTML = "00:00:00"
-        
         element.innerHTML = "Time out!"
         document.getElementById("submit").innerHTML="Set again!"
         audio.play()
@@ -50,6 +49,6 @@ function countdown(time){
         document.getElementById("submit").disabled = false;
       }
     }, 1000);
-    
+  }
   
 }
